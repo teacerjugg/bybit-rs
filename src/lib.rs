b10617fb4e32e17eb09d86e6fd5965172ab0cf69
tnagao <tnagao+git@pm.me>
2021-04-06 22:17:17 +0900

    Add prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod enums;
+pub mod prelude;
 pub mod store;
 pub mod structs;
 pub mod websocket;
---
+++ src/prelude.rs
@@ -0,0 +1,4 @@
+pub use crate::enums::Endpoint;
+pub use crate::structs::{Limit, OrderBook, Record, API};
+pub use crate::websocket::{Websocket, WebsocketBuilder, WebsocketResponse};
+// pub use crate::store::;
